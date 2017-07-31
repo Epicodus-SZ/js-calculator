fd692aeb721ffcc86946a7e2ee05996057ee436e
Steve Zaske and Witty Chang <steve+witty@>
2017-07-31 16:00:04 -0700

    add SASS support to our Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,8 @@ var lib = require('bower-files')({
   }
 });
 var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 var buildProduction = utilities.env.production;
 
 gulp.task('jsBrowserify', ['concatInterface'], function() {
@@ -46,6 +48,7 @@ gulp.task("build", ['clean'], function(){
     gulp.start('jsBrowserify');
   }
     gulp.start('bower');
+    gulp.start('cssBuild');
 });
 
 gulp.task("clean", function(){
@@ -88,6 +91,7 @@ gulp.task('serve', ['jsBuild'],  function() {
   gulp.watch(['js/*.js'], ['jsBuild']);
   gulp.watch(['bower.json'], ['bowerBuild']);
   gulp.watch(['*.html'], ['htmlBuild']);
+  gulp.watch(["scss/*.scss"], ['cssBuild']);
 
 });
 
@@ -101,4 +105,13 @@ gulp.task('bowerBuild', ['bower'], function(){
 
 gulp.task('htmlBuild', function() {
   browserSync.reload();
+});
+
+gulp.task('cssBuild', function() {
+  return gulp.src(['scss/*.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./build/css'))
+    .pipe(browserSync.stream());
 });
