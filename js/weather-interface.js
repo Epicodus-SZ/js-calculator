961b4f6a6c862ee27a1174eefdc13f31f43669c1
Steve Zaske and Witty Chang <steve+witty@>
2017-07-31 12:02:08 -0700

    Add AJAX calls.

---
+++ js/weather-interface.js
@@ -0,0 +1,12 @@
+var apiKey = "a6ee864aa8d0ccce38a1fef0ceeff11e";
+
+$(document).ready(function() {
+  $('#weather-location').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $('.showWeather').text("The city you have chosen is " + city + ".");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      console.log(response);
+    });
+  });
+});
